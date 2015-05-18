33aaefb8c19408543b9044dee9182a5d4655d409
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-18 04:12:52 +0200

    Initial import.

---
+++ new_dist.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+TARGET=target
+SUITE=wheezy
+MIRROR=http://http.debian.net/debian/
+ARCH=armhf
+VARIANT=minbase
+
+# create new dir first
+mkdir -p "${TARGET}"
+
+# setup apt mirrors
+cat <<EOF > "${TARGET}/etc/apt/sources.list"
+deb http://http.debian.net/debian/ jessie main contrib non-free
+EOF
+
+debootstrap --verbose --foreign --arch "${ARCH}" --variant="${VARIANT}" "${SUITE}" "${TARGET}" "${MIRROR}"
