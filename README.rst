a63dca2847950e2215e91246278596b0a27f1f8e
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-19 00:04:57 +0200

    Working firmware extraction.

--- README.rst
+++ README.rst
@@ -6,6 +6,13 @@ Pi or use one generated with it while understanding what exactly went into it.
 
 
 
+Issues
+------
+
+The ``raspberrypi-firmware-nokernel`` package unfortunately seems to be either
+obsolete or buggy; the .postinst scripts deletes all the firmware it installed
+from /boot/firmware.xchat
+
 
 Sources
 -------
@@ -16,3 +23,4 @@ Information taken from:
 * https://wiki.debian.org/Multistrap
 * http://sjoerd.luon.net/posts/2015/02/debian-jessie-on-rpi2/
 * https://unix.stackexchange.com/questions/41889/how-can-i-chroot-into-a-filesystem-with-a-different-architechture
+* https://raspberrypi.stackexchange.com/questions/10442/what-is-the-boot-sequence
--- new_dist.sh
+++ new_dist.sh
@@ -8,20 +8,45 @@ umask 022
 
 # ensure we have a valid target directory
 if [ "$#" -ne 1 ] || ! [ "$1" ]; then
-  echo "usage: $(basenaem $0) TARGETDIR"
+  echo "usage: $(basename $0) TARGETDIR"
   exit 1;
 fi;
 
 TARGETDIR="$1"
+
+### STEP 0: download firmware
+FW_REV=$(git ls-remote https://github.com/raspberrypi/firmware master | cut -f1)
+FW_SOURCE=https://github.com/raspberrypi/firmware/archive/${FW_REV}.zip
+FW_PATH=firmware-${FW_REV}
+FW_ZIP=${FW_PATH}.zip
+
+if ! [ -e "${FW_ZIP}" ]; then
+  echo "Downloading latest firmware (master branch): $FW_ZIP"
+  curl -L -o ${FW_ZIP} ${FW_SOURCE}
+fi;
+
+
+### STEP 1: bootstrapping with multistrap and adding files
 CONFFILE=raspi.config
-IN_CHROOT="sudo chroot ${TARGETDIR} /usr/bin/env -i PATH=/bin:/usr/bin:/sbin:/usr/sbin DEBIAN_FRONTEND=noninteractive"
 MACHINE_ID_FILE=${TARGETDIR}/etc/machine-id
 
-# we need a
+# we need qemu for chroot
 QEMU_STATIC=$(which qemu-arm-static)
 QEMU_CHROOT=${TARGETDIR}/usr/bin/qemu-arm-static
+IN_CHROOT="sudo chroot ${TARGETDIR} /usr/bin/env -i PATH=/bin:/usr/bin:/sbin:/usr/sbin DEBIAN_FRONTEND=noninteractive"
+
+# and extract firmware
+FIRMWARE_DIR=${TARGETDIR}/boot/firmware
 
 multistrap -f ${CONFFILE} -d ${TARGETDIR}
+
+mkdir -p "${FIRMWARE_DIR}"
+
+# extract firmware into /boot/firmware
+unzip -o -j -d "${FIRMWARE_DIR}" "${FW_ZIP}" "${FW_PATH}/boot/*.bin" "${FW_PATH}/boot/*.dat" "${FW_PATH}/boot/*.elf"
+
+# copy over config.txt
+cp config.txt "${FIRMWARE_DIR}/config.txt"
 
 # copy qemu into chroot to make it possible to run stuff
 cp ${QEMU_STATIC} ${QEMU_CHROOT}
@@ -29,6 +54,8 @@ cp ${QEMU_STATIC} ${QEMU_CHROOT}
 # create an empty machine id, otherwise systemd.deb will try to generate one
 # we erase it afterwards
 echo '0123456789abcdef0123456789abcdef' > ${MACHINE_ID_FILE}
+
+### STEP 2: chroot stage-two
 
 # FIXME: fakechroot would be nice here
 sudo chown root.root ${TARGETDIR} -R
@@ -40,3 +67,4 @@ ${IN_CHROOT} /usr/bin/dpkg --configure -a
 
 # files no longer required
 sudo rm -f ${MACHINE_ID_FILE} ${QEMU_CHROOT}
+
