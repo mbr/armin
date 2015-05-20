a3f0a6886f7ffe07cf6205533fc133ede876bae9
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-20 20:43:49 +0200

    Added chroot_copy.

--- new_dist.sh
+++ new_dist.sh
@@ -14,6 +14,10 @@ run_hooks() {
   done;
 }
 
+chroot_copy() {
+  install -m 0644 -D "${HOOKDIR}/files/$1" "${CHROOTDIR}/$2/$1"
+}
+
 # To ensure all files end up with the correct permissions, set the umask to the
 # standard of 022
 umask 022
